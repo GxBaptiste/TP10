57dc27cd40228240bbd1bd073c7dda653e57231b
Baptiste <smaug.exe@gmail.com>
2019-01-04 10:39:32 +0100

    test master mergeé

--- src/TP11/Main.java
+++ src/TP11/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws ImpressionHorsLimiteException {
 
 
-		// FigureUtil.getRandomRond().affiche();
+		// FigureUtil.getRandomRond().affiche() elelelelel;
 		// FigureUtil.getRandomRectangle().affiche();
 		for (int i = 0; i < 10; i++) {
 			System.out.println("\nRANDOM FIGURE : \n");
