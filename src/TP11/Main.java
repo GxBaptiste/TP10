2d100581a4c949cc08904fab6006dd58cc53cd54
Baptiste <smaug.exe@gmail.com>
2019-01-04 10:34:16 +0100

    commit de la branche master

--- src/TP11/Main.java
+++ src/TP11/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) throws ImpressionHorsLimiteException {
 
-		// FigureUtil.getRandomRond().affiche();
+		// FigureUtil.getRandomRond().affiche()      ;
 		// FigureUtil.getRandomRectangle().affiche();
 		for (int i = 0; i < 10; i++) {
 			System.out.println("\nRANDOM FIGURE : \n");
